851539c7b8299231c4511bf001e88bd4ff1e2219
Alex Wermer-Colan <alwermercolan@gmail.com>
2020-06-01 11:52:47 -0400

    Uploading API_Key.r file
    API key only needs to be added to API_Key.r script, which the main pipeline imports

---
+++ API_Keys.r
@@ -0,0 +1,4 @@
+#API_ID <- "Authenticator ID"
+#API_Secret <- "Authenticator Secret"
+API_ID <- "360178073531-6lc5l0fbculae92lgd7mu6kgb9vu0rk3.apps.googleusercontent.com"
+API_Secret <- "fNjTwXSrn7u_Wlz04-oZqle_"
