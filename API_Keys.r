904f8b8c96647d0d6bbe4a90ccccb010a6ef5575
Alex Wermer-Colan <alwermercolan@gmail.com>
2020-06-01 11:55:40 -0400

    Updated API_Keys.r

--- API_Keys.r
+++ API_Keys.r
@@ -1,4 +1,2 @@
-#API_ID <- "Authenticator ID"
-#API_Secret <- "Authenticator Secret"
-API_ID <- "360178073531-6lc5l0fbculae92lgd7mu6kgb9vu0rk3.apps.googleusercontent.com"
-API_Secret <- "fNjTwXSrn7u_Wlz04-oZqle_"
+API_ID <- "OAuthenticator ID"
+API_Secret <- "OAuthenticator Secret"
