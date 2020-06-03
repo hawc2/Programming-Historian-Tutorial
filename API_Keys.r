eaa63bf471aa2102b7dd00fc3339edf59d5b9344
Alex Wermer-Colan <alwermercolan@gmail.com>
2020-06-03 14:32:22 -0400

    Updating date script

--- API_Keys.r
+++ API_Keys.r
@@ -1,2 +1,2 @@
-API_ID <- "OAuthenticator ID"
-API_Secret <- "OAuthenticator Secret"
+API_ID <- "277563602415-ga37vt6i3hl6iedm5fi0sr3ne2fndtd2.apps.googleusercontent.com"
+API_Secret <- "jNUpGFT-uiT9m_v7gBr1OOvF"
